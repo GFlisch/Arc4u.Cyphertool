3e31e643fb7564d6d8774d5bc405d3155d5b4ac0
Gilles Flisch <info@arc4u.net>
2024-07-27 00:15:50 +0200

    Add Encryption from text or file

---
+++ src/CertificateHelper.cs
@@ -0,0 +1,87 @@
+﻿// Licensed to the Arc4u Foundation under one or more agreements.
+// The Arc4u Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+using Arc4u.Security;
+using Arc4u.Security.Cryptography;
+using FluentResults;
+using Microsoft.Extensions.Logging;
+
+namespace Arc4u.Encryptor
+{
+    internal class CertificateHelper
+    {
+        public static Result<X509Certificate2> GetCertificate([DisallowNull] string cert, string? password, string? storeName, string? storeLocation, ILoggerFactory loggerFactory)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(cert);
+
+            var logger = loggerFactory.CreateLogger<CertificateHelper>();
+            var x509Logger = loggerFactory.CreateLogger<X509CertificateLoader>();
+
+            // Certificate is coming from the store if the certOption.Value() does not contain a file name ending with .pfx
+            bool fromCertStore = !cert.EndsWith(".pfx");
+
+            if (fromCertStore)
+            {
+                return GetCertificateFromStore(cert, storeName, storeLocation, logger, x509Logger);
+            }
+            else
+            {
+                return GetCertificateFromFile(cert, password, logger);
+            }
+        }
+
+        private static Result<X509Certificate2> GetCertificateFromFile(string cert, string? password, ILogger<CertificateHelper> logger)
+        {
+            if (!File.Exists(cert))
+            {
+                return Result.Fail($"The certificate file {cert} does not exist!");
+            }
+
+            if (File.Exists(cert))
+            {
+                return Result.Try(() => new X509Certificate2(cert, password));
+            }
+
+            return Result.Fail($"The certificate file {cert} does not exist!");
+        }
+        private static Result<X509Certificate2> GetCertificateFromStore(string cert, string? storeName, string? storeLocation, ILogger<CertificateHelper> logger, ILogger<X509CertificateLoader> x509Logger)
+        {
+            var certInfo = new CertificateInfo
+            {
+                Name = cert
+            };
+
+            if (!string.IsNullOrWhiteSpace(storeName))
+            {
+                try
+                {
+                    certInfo.StoreName = Enum.Parse<StoreName>(storeName, true);
+                }
+                catch (Exception)
+                {
+
+                    return Result.Fail($"{storeName} is not a valid store!");
+                }
+                
+            }
+
+            if (!string.IsNullOrWhiteSpace(storeLocation))
+            {
+                try
+                {
+                    certInfo.Location = Enum.Parse<StoreLocation>(storeLocation, true);
+                }
+                catch (Exception)
+                {
+
+                    return Result.Fail($"{storeLocation} is not a valid location!");
+                }
+            }
+
+            return Result.Try(() => new X509CertificateLoader(x509Logger).FindCertificate(certInfo));
+        }
+
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,14 @@
 ﻿// Licensed to the Arc4u Foundation under one or more agreements.
 // The Arc4u Foundation licenses this file to you under the MIT license.
 
-using System.Security.Cryptography.X509Certificates;
-using Arc4u.Encryptor;
+using Arc4u.Results;
+using Arc4u.Results.Logging;
+using Arc4u.Security.Cryptography;
+using FluentResults;
 using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging;
 
-namespace MyEncryptionTool
+namespace Arc4u.Encryptor
 {
     class Program
     {
@@ -19,57 +22,100 @@ namespace MyEncryptionTool
 
             app.HelpOption();
 
-            var certOption = app.Option("-c", "The name of the certificate", CommandOptionType.SingleValue)
-                .IsRequired();
+            using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+                    builder.AddSimpleConsole(options =>
+                    {
+                        options.IncludeScopes = true;
+                        options.SingleLine = true;
+                        options.TimestampFormat = "HH:mm:ss ";
+                    }));
 
-            var textOption = app.Option("-t", "The text to encrypt", CommandOptionType.SingleValue)
-                .IsRequired();
+            ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
 
-            var decrypt = app.Option<bool>("-d", "Decrypt if specified otherwhise encrypt.", CommandOptionType.NoValue);
+            Result.Setup(setup =>
+            {
+                setup.Logger = new FluentLogger(loggerFactory.CreateLogger<FluentLogger>());
+            });
+
+            var certOption = app.Option("-c | --certificate", "The name of the certificate", CommandOptionType.SingleValue)
+                                .IsRequired();
+
+            var textOption = app.Option("-t | --text", "The text to encrypt", CommandOptionType.SingleValue);
+
+            var decrypt = app.Option<bool>("-d | --decrypt", "Decrypt if specified otherwhise encrypt.", CommandOptionType.NoValue);
+
+            var passwordOption = app.Option("-p | --password", "The password to use for the file pfx certificate", CommandOptionType.SingleValue);
+
+            var fileOption = app.Option("-f | --file", "The file name of the text to encrypt. if the --file option is used the --text one is not used!", CommandOptionType.SingleValue);
+
+            var nameOption = app.Option("-n | --storename", "The name of the folder where the certificate is stored in a Keychain or Certificate Store.", CommandOptionType.SingleValue);
+
+            var locationOption = app.Option("-l | --storelocation", "The location where the certificate is stored in a Keychain or Certificate Store. Like on Windows: CurrentUser or LocalMachine. Default is CurrentUser!", CommandOptionType.SingleValue);
 
             app.OnExecute(() =>
             {
-                if (string.IsNullOrWhiteSpace(certOption.Value()))
-                {
-                    Console.WriteLine("A certificate is needed");
-                }
                 string certificateName = certOption.Value()!;
+                bool stopOnErrors = false;
 
-                if (string.IsNullOrWhiteSpace(textOption.Value()))
+                if (string.IsNullOrWhiteSpace(textOption.Value()) && string.IsNullOrWhiteSpace(fileOption.Value()))
                 {
-                    Console.WriteLine("A text is needed");
+                    logger.LogError("A text -t | --text or a file -f | --file is needed");
+                    stopOnErrors = true;
                 }
-                string text = textOption.Value()!;
 
-                try
+                if (!string.IsNullOrWhiteSpace(textOption.Value()) && !string.IsNullOrWhiteSpace(fileOption.Value()))
                 {
-                    // Load the certificate from the specified file
-                    X509Certificate2 certificate = Certificate.FindCertificate(certificateName);
-
-                    if (decrypt.HasValue())
-                    {
-                        string decryptedText = certificate.Decrypt(text);
-
-                        Console.WriteLine($"Encrypted string: '{decryptedText}'");
-
-                        return 0;
-                    }
-                    // Convert the encrypted bytes to a base64 string for easy transmission
-                    string encryptedString = certificate.Encrypt(text);
-
-                    Console.WriteLine($"Encrypted string: '{encryptedString}'");
-
-                    return 0;
+                    logger.LogError("Only a text or a file must be given.");
+                    stopOnErrors = true;
                 }
-                catch (Exception ex)
+
+                if (stopOnErrors)
                 {
-                    Console.WriteLine(ex.ToString());   
-                    Console.WriteLine($"An error occurred: {ex.Message}");
                     return 1;
                 }
+
+                string text = fileOption.HasValue() ? File.ReadAllText(fileOption.Value()!) : textOption.Value()!;
+
+                var result = Result.Ok(string.Empty);
+
+                CertificateHelper.GetCertificate(certOption.Value()!, passwordOption.Value(), nameOption.Value(), locationOption.Value(), loggerFactory)
+                   .LogIfFailed()
+                   .OnSuccessNotNull(certificate =>
+                   {
+                       // Decryption
+                       if (decrypt.HasValue())
+                       {
+                           result = Result.Try(() => certificate.Decrypt(text));
+                       }
+                       else // encryption
+                       {
+                           result = Result.Try(() => certificate.Encrypt(text));
+                       }
+                   });
+
+                var returnValue = 1;
+
+                result
+                    .LogIfFailed()
+                    .OnSuccess(text =>
+                    {
+                        if (decrypt.HasValue())
+                        {
+                            logger.LogInformation($"Decrypted text: '{text}'");
+                        }
+                        else
+                        {
+                            logger.LogInformation($"Encrypted text: '{text}'");
+                        }
+                        returnValue = 0;
+                    });
+
+                return returnValue;
             });
 
             return app.Execute(args);
         }
+
+
     }
 }
